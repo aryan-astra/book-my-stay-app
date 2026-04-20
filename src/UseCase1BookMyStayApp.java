3b1b34c72ca31b2dc2f2c01833a5bc71897df03a
Aryan Shukla <as6977@srmist.edu.in>
2026-04-20 11:05:04 +0530

    feat: add UC1 book my stay solution

---
+++ src/UseCase1BookMyStayApp.java
@@ -0,0 +1,9 @@
+public class UseCase1BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("UC1 - Welcome");
+        System.out.println("Welcome to Book My Stay!");
+        System.out.println("Plan your stay with quick room search and booking.");
+
+        System.out.println("\nSample output complete for UC1.");
+    }
+}
